74b5324b3a9e549cd990026bfdd64b8911217750
abdulkabeer-ak <128490431+abdulkabeer-ak@users.noreply.github.com>
2023-03-21 18:34:09 +0500

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+name = "john smith"
+
+print (name)
+
+print(name[1])
+print(name[-2])
+print(name[1:-1])
+print(len(name))
+print(name.title())
+print(name.strip())
+print(name.find("smith"))
+print(name.replace("j","k"))
+print(10/3)
+print(10//3)
+print(10**3)
+x = 1
+x += 2
+print(x)
+print(float(1))
+print(10 == "10")
+print("bag" > "apple")
+print(not(True or False))
+print(range(1,10,2))
+
+
+# In[ ]:
+
+
+
+
